a2fb2107f0c9b92ac38ac4c4ecd2888e68cb1b3a
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-09-07 11:56:27 +0800

    fix: stricter match urls

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         name: "GitHub Feed Back",
         icon: "https://github.githubassets.com/favicons/favicon.svg",
         namespace: "https://github.com/Sec-ant",
-        match: ["https://github.com/*"],
+        match: ["https://github.com/", "https://github.com/dashboard"],
         "run-at": "document-start",
       },
     }),
